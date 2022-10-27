975b7200970128e48c0931852b44a5fffbfa41db
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-10-27 18:28:36 +0200

    Change component footer and test

--- src/infrastructure/components/footer/footer.test.tsx
+++ src/infrastructure/components/footer/footer.test.tsx
@@ -1,19 +1,12 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Footer } from './footer';
 
 describe('Given Footer component', () => {
     describe('When we render the component', () => {
-        test('Then it should display "ISDI"', () => {
+        test('Then it should display "Aldana"', () => {
             render(<Footer />);
-            const element = screen.getByText(/ISDI/i);
+            const element = screen.getByText(/Aldana/i);
             expect(element).toBeInTheDocument();
         });
-        // test('Then it should display today date', () => {
-        //     render(<Footer />);
-        //     const expectedDate = new Date().toLocaleString();
-        //     const element = screen.getByText(expectedDate);
-        //     expect(element).toBeInTheDocument();
-        // });
     });
 });
--- src/infrastructure/components/footer/footer.tsx
+++ src/infrastructure/components/footer/footer.tsx
@@ -1,11 +1,8 @@
 import styles from './footer.module.css';
 export function Footer() {
-    const date = new Date();
-
     return (
         <footer className={styles.footer}>
-            <address>ISDI</address>
-            <p>{date.toLocaleString()}</p>
+            <address>Aldana Arcidiacono</address>
         </footer>
     );
 }
