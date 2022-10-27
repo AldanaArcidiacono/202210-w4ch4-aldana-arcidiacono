afc9deace6c1f7ea2af173699e4c665dce64daec
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-10-27 20:26:42 +0200

    Add list and fix to show the robots

---
+++ src/features/robots/components/robot.list/robot.list.tsx
@@ -0,0 +1,18 @@
+import { ROBOTS } from '../../models/data';
+import { Robot } from '../../models/robot';
+import { RobotItem } from '../robot.item/robot.item';
+import styles from './robot.list.module.css';
+
+export function RobotList() {
+    const title = 'Robots';
+    return (
+        <section>
+            {/* <h2 className="title__robot">{title}</h2> */}
+            <ul>
+                {ROBOTS.map((item: Robot) => (
+                    <RobotItem item={item}></RobotItem>
+                ))}
+            </ul>
+        </section>
+    );
+}
--- src/features/robots/models/data.ts
+++ src/features/robots/models/data.ts
@@ -1,7 +1,7 @@
 import { Robot } from './robot';
 
-export const TASKS: Array<Robot> = [
+export const ROBOTS: Array<Robot> = [
     new Robot('P3P3', 'https://robohash.org/9', 6, 10, '31/12/1999'),
     new Robot('Ernesto', 'https://robohash.org/29', 5, 4, '15/10/2013'),
-    new Robot('Elena', 'https://robohash.org/19', 0, 0, '20/06/2005'),
+    new Robot('Elena', 'https://robohash.org/19', 10, 6, '20/06/2005'),
 ];
