7f1b9af0bb32f15cc0551dca33ac2fd84e35270d
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-10-27 18:29:59 +0200

    Change header components and test

--- src/infrastructure/components/header/header.test.tsx
+++ src/infrastructure/components/header/header.test.tsx
@@ -4,13 +4,13 @@ import { MemoryRouter as Router } from 'react-router-dom';
 
 describe('Given Header component', () => {
     describe('When we render the component', () => {
-        test('Then it should display "Learning React"', () => {
+        test('Then it should display "Robots"', () => {
             render(
                 <Router>
                     <Header />
                 </Router>
             );
-            const element = screen.getByText(/Learning React/i); // Expresion regular
+            const element = screen.getByText(/Robots/i); // Expresion regular
             expect(element).toBeInTheDocument();
         });
     });
